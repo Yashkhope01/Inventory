7bef72b21bfc10fb77dedf3308c4b23bb8ed1600
Yash Khope <yashkhope123@gmail.com>
2026-01-18 15:21:00 +0530

    Add handler alias in api/index.py

--- api/index.py
+++ api/index.py
@@ -4,4 +4,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amazon.settings")
 
 app = get_wsgi_application()
+handler = app  # Vercel may look for `handler`
 
